1ca46d109886a04c7a3c5095460cc09533ca0639
unknown <holdcroftkari@gmail.com>
2020-03-04 12:01:57 +0100

    Task 10 created a recipes smart component and recipe dumb component

---
+++ src/components/Recipe.vue
@@ -0,0 +1,48 @@
+<template>
+  <article class="recipe">
+    <h2>
+      <a class="recipe-link" v-bind:href="url">{{title}}</a>
+      <img class="recipe-thumbnail" v-bind:src="thumbnail" />
+      <ul class="ingredients">
+        <li v-for="(ingredient, index) in ingredients" :key="index" v-text="ingredient" />
+      </ul>
+    </h2>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Recipe",
+  props: {
+    title: String,
+    tooltip: String,
+    url: String,
+    ingredients: Array,
+    thumbnail: String
+  }
+};
+</script>
+
+<style>
+.recipe {
+  display: flex;
+  flex-flow: column;
+  width: 280px;
+}
+
+.ingredients {
+  text-align: left;
+}
+
+.recipe-thumbnail {
+  width: 120px;
+  height: auto;
+}
+
+.recipe-link {
+  display: block;
+  color: #42b983;
+  text-decoration: none;
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/components/Recipes.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <Recipe
+      v-for="(recipe, index) in recipes"
+      v-bind:key="index"
+      v-bind:title="recipe.title"
+      v-bind:tooltip="`Go to ${recipe.title} page.`"
+      v-bind:url="recipe.href"
+      v-bind:ingredients="recipe.ingredients.split(',')"
+      v-bind:thumbnail="recipe.thumbnail"
+    />
+  </section>
+</template>
+
+<script>
+import Recipe from "./Recipe";
+export default {
+  name: "Recipes",
+  props: {
+    recipes: Array
+  },
+  components: { Recipe }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+section {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+</style>
